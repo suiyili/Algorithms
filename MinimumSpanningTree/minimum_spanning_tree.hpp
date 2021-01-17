5c03e1ab1627533d2c48b1cd93de64aafe6668db
suiyi li <suiyili.sli@gmail.com>
2021-01-17 17:12:38 +0000

    minimum spanning tree

---
+++ MinimumSpanningTree/minimum_spanning_tree.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <queue>
+#include <unordered_set>
+
+#include "tree_retriever.hpp"
+#include "tree_type.hpp"
+
+using namespace tree::generic;
+
+namespace tree::spanning_tree {
+
+  enum class spanning_type : char
+  {
+    maximum = 1,
+    minimum = -1
+  };
+
+  using connect_set = std::pmr::unordered_set<connect>;
+
+  connect_set get_spanning_tree(const tree_retriever& tree, spanning_type type);
+
+}  // namespace tree::spanning_tree
--- PatternMatch/resource_manager.hpp
+++ PatternMatch/resource_manager.hpp
@@ -5,14 +5,14 @@
 namespace pattern_match {
 class resource_manager final {
  public:
-  explicit resource_manager(size_t capacity);
-  template<typename T>
-  auto get_allocator()
-  {
+  template <typename T>
+  static auto get_allocator() {
     static resource_manager resource(4096);
-    return std::pmr::polymorphic_allocator<T>(&resource.buffer_);
+    return std::pmr::polymorphic_allocator<T>(&resource.pool_);
   }
+
  private:
+  explicit resource_manager(size_t capacity);
   std::pmr::monotonic_buffer_resource buffer_;
   std::pmr::unsynchronized_pool_resource pool_;
 };
